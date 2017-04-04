59ae02a1388f23bbcad178231b24325eb183979c
Duncan Marsh <dwolfm@gmail.com>
2017-04-04 16:44:24 -0700

    added slugbyte index.html

--- class-02-intro-js-html-css-arrays/class-code/app.js
+++ class-02-intro-js-html-css-arrays/class-code/app.js
@@ -2,9 +2,9 @@
 
 console.log('lulwat this rocks!');
 
-var sushi = prompt('do you think i like sushi?');
-if ( sushi.trim().toLowerCase()  == 'yes') {
-  alert('you were correct');
-} else {
-  alert('you are clueless about me');
-}
+//var sushi = prompt('do you think i like sushi?');
+//if ( sushi.trim().toLowerCase()  == 'yes') {
+//  alert('you were correct');
+//} else {
+//  alert('you are clueless about me');
+//}
